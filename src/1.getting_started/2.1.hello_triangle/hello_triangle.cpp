40e5382130715c537c4efbe125c1e54601dd1b0c
SanjaVidakovic <81044687+SanjaVidakovic@users.noreply.github.com>
2022-10-21 20:08:10 +0200

    komentari 2.cas

--- src/1.getting_started/2.1.hello_triangle/hello_triangle.cpp
+++ src/1.getting_started/2.1.hello_triangle/hello_triangle.cpp
@@ -79,7 +79,7 @@ int main()
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
     // fragment shader
-    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER); 
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
     // check for shader compile errors
@@ -93,7 +93,7 @@ int main()
     int shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
+    glLinkProgram(shaderProgram);//ovim spajamo vertex shader i fragment shader u jedno-shaderProgram
     // check for linking errors
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
@@ -106,25 +106,30 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
+        //aPos           aOffset
         -0.5f, -0.5f, 0.0f, // left  
          0.5f, -0.5f, 0.0f, // right 
          0.0f,  0.5f, 0.0f  // top   
     }; 
 
     unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
+    glGenVertexArrays(1, &VAO);//pravi 1 i vraca ga u VAO 
     glGenBuffers(1, &VBO);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);//za trenutno aktivan array buffer napravi ,alocira 24bajta
+    //static znaci da je statican, a draw da ce da ga crta
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);//vrteks je jedno polje, on ima pozicije(atribut(e))
+    //3 je koliko ima vrednosti, float kog su tipa, false da ne zelimo da opengl normalizuje podatke
+    //3*sizeof(float) je VELICINA VRTEKSA KOJI SMO KREIRALI(ne samo dela), void*0 je od kog bajta pocinje
+    //STRIDE je parametar koliko od pocetka se zauzima bajtova
     glEnableVertexAttribArray(0);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    glBindBuffer(GL_ARRAY_BUFFER, 0); //pokazivac se postavi na null, trenutno aktivan objekat array deaktiviraj 
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
@@ -150,7 +155,7 @@ int main()
         // draw our first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 3);//iscrtavanje
         // glBindVertexArray(0); // no need to unbind it every time 
  
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
